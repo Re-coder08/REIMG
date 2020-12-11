600a96946618ab2f948088b64442d4523142ac7c
Recoder-08 <revanthreddy286@gmail.com>
2020-12-11 05:00:44 -0600

    fixed shape issue with convo function.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from PIL import Image
 
 test_image = "test.png"
 
@@ -13,7 +14,7 @@ def open_image(image):
 
 
 def convo(aa, KERNEL):
-
+    W, H = aa.shape
     k_size = len(KERNEL)
     off = k_size - 1
 
@@ -29,13 +30,12 @@ def convo(aa, KERNEL):
                 c.append(padded_aa[ i : i + k_size  ,  j : j + k_size])
 
     cr = [np.sum(np.multiply(KERNEL, sub)) for sub in c]
-
     final_convo = []
     count = 0
     temp = []
     for l in range(len(cr)):
         count += 1
-        if count <= 4:
+        if count <= W-1:
             temp.append(cr[l])
         else:
             final_convo.append(temp)
@@ -43,19 +43,20 @@ def convo(aa, KERNEL):
             temp = []
 
 
-    # print("final_convo : ", final_convo)
-
+    final_convo = np.asarray(final_convo)
+    # print("final_convo : ", final_convo.shape )
     return final_convo
 
 
 def main():
     image_data = open_image(test_image)
     # image_data = np.asarray([[105, 102, 100, 97, 96], [103, 99, 103, 101, 102], [101, 98, 104, 102, 100], [99, 101, 106, 104, 99], [104, 104, 104, 100, 98]])
+    print(image_data.shape)
     KERNEL = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     KERNEL = np.flipud(np.fliplr(KERNEL))
 
-    sharp_img = convo(image_data[:5, :5], KERNEL)
-    print(sharp_img)
-
+    sharp_img = convo(image_data, KERNEL)
+    img = Image.fromarray(sharp_img)
+    img.show()
 if __name__ == '__main__':
     main()
